c9eedac08615d3eee0413a946ec3938f5d96ff23
Kerim-Personal <batinkerimkaraman@gmail.com>
2025-08-09 21:50:07 +0300

    Improvements UI/UX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.codenzi.mathlabs"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
